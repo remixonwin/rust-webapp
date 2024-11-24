f530cf48b55cd02b50ef50dfe03e8d3dacb76fe7
remixonwin <remixonwin@gmail.com>
2024-11-24 12:13:16 -0600

    fix: remove unused not_found function

--- src/handlers/error.rs
+++ src/handlers/error.rs
@@ -1,11 +1,5 @@
 use actix_web::HttpResponse;
 
-pub async fn not_found() -> HttpResponse {
-    HttpResponse::NotFound().finish()
-}
-
 pub async fn method_not_allowed() -> HttpResponse {
-    HttpResponse::MethodNotAllowed()
-        .insert_header(("Allow", "GET"))
-        .finish()
+    HttpResponse::MethodNotAllowed().finish()
 }
